84d841bfe14ec6dfe749218fbc52c78edb68200a
AL Drin <143868630+l0n3m4n@users.noreply.github.com>
2024-07-03 02:24:38 +0800

    Add files via upload

---
+++ bin2shell.py
@@ -0,0 +1,133 @@
+import argparse
+
+class Color:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
+
+def format_shellcode_c(input_file):
+    ctr = 1
+    maxlen = 15
+    formatted_shellcode = "\""
+
+    with open(input_file, "rb") as f:
+        byte = f.read(1)
+        while byte:
+            formatted_shellcode += "\\x" + byte.hex()
+            if ctr == maxlen:
+                formatted_shellcode += "\" \n\""
+                ctr = 0
+            ctr += 1
+            byte = f.read(1)
+    formatted_shellcode += "\""
+    
+    return formatted_shellcode
+
+def format_shellcode_cpp(input_file):
+    formatted_shellcode = "const char shellcode[] = {"
+
+    with open(input_file, "rb") as f:
+        byte = f.read(1)
+        while byte:
+            formatted_shellcode += "0x" + byte.hex() + ","
+            byte = f.read(1)
+    
+    formatted_shellcode = formatted_shellcode.rstrip(',') + "};"
+    
+    return formatted_shellcode
+
+def format_shellcode_csharp(input_file):
+    formatted_shellcode = "byte[] shellcode = new byte[] {"
+
+    with open(input_file, "rb") as f:
+        byte = f.read(1)
+        while byte:
+            formatted_shellcode += "0x" + byte.hex() + ","
+            byte = f.read(1)
+    
+    formatted_shellcode = formatted_shellcode.rstrip(',') + "};"
+    
+    return formatted_shellcode
+
+def format_shellcode_asm(input_file):
+    formatted_shellcode = "section .text\n"
+    formatted_shellcode += "global _start\n\n"
+    formatted_shellcode += "_start:\n"
+    formatted_shellcode += "\tjmp shellcode\n\n"
+    formatted_shellcode += "shellcode:\n"
+    
+    with open(input_file, "rb") as f:
+        byte = f.read(1)
+        while byte:
+            formatted_shellcode += f"\tdb 0x{byte.hex()}\n"
+            byte = f.read(1)
+    
+    return formatted_shellcode
+
+def main():
+    banner = f"""{Color.GREEN}
+  ,--.   ,--.         ,---.        ,--.            ,--.,--. 
+  |  |-. `--',--,--, '.-.  \ ,---. |  ,---.  ,---. |  ||  | 
+  | .-. ',--.|      \ .-' .'(  .-' |  .-.  || .-. :|  ||  | 
+  | `-' ||  ||  ||  |/   '-..-'  `)|  | |  |\   --.|  ||  | 
+   `---' `--'`--''--''-----'`----' `--' `--' `----'`--'`--' 
+      Author: @l0n3m4n / Shellcode Generator / v1.2
+{Color.RESET}"""
+
+    parser = argparse.ArgumentParser(description="Shellcode Generator (C,C#,CPP,ASM)",
+                                    epilog='Example usage: python3 shellcode_formatter.py -s shellcode.bin -c shellcode_c.txt')
+    print(banner)
+    parser.add_argument('-s', '--shellcode', metavar='shellcode.bin', required=True, help='Input shellcode binary file')
+    parser.add_argument('-c', '--c_output', metavar='c.txt', help='Output file for C formatted shellcode')
+    parser.add_argument('-cpp', '--cpp_output', metavar='cpp.txt', help='Output file for C++ formatted shellcode')
+    parser.add_argument('-cs', '--cs_output', metavar='cs.txt', help='Output file for C# formatted shellcode')
+    parser.add_argument('-asm', '--asm_output', metavar='asm.txt', help='Output file for Assembly (NASM) formatted shellcode')
+
+    args = parser.parse_args()
+
+    input_file = args.shellcode
+
+    if args.c_output:
+        # Format shellcode for C
+        c_code = format_shellcode_c(input_file)
+        print(f"\n{Color.GREEN}Formatted C Shellcode:\n")
+        print(c_code)
+
+        with open(args.c_output, 'w') as f:
+            f.write(c_code)
+        print(f"\nSaved C formatted shellcode to {args.c_output}")
+
+    if args.cpp_output:
+        # Format shellcode for C++
+        cpp_code = format_shellcode_cpp(input_file)
+        print("\nFormatted C++ Shellcode:\n")
+        print(cpp_code)
+
+        with open(args.cpp_output, 'w') as f:
+            f.write(cpp_code)
+        print(f"\nSaved C++ formatted shellcode to {args.cpp_output}")
+
+    if args.cs_output:
+        # Format shellcode for C#
+        csharp_code = format_shellcode_csharp(input_file)
+        print("\nFormatted C# Shellcode:\n")
+        print(csharp_code)
+
+        with open(args.cs_output, 'w') as f:
+            f.write(csharp_code)
+        print(f"\nSaved C# formatted shellcode to {args.cs_output}")
+
+    if args.asm_output:
+        # Format shellcode for ASM
+        asm_code = format_shellcode_asm(input_file)
+        print("\nFormatted ASM Shellcode:\n")
+        print(asm_code)
+
+        with open(args.asm_output, 'w') as f:
+            f.write(asm_code)
+        print(f"\nSaved ASM formatted shellcode to {args.asm_output}{Color.RESET}")
+
+if __name__ == "__main__":
+    main()
